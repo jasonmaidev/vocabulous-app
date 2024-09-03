2b5333577b28bf0daa15b917cbdaed1714dd085f
JFMai90 <jasonmaif@gmail.com>
2024-09-03 03:55:27 -0700

    update api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const apiUrl = "https://slaystyle-api-production-dc70.up.railway.app"
+const apiUrl = "vocabulous-api-production.up.railway.app"
 // get api endpoint url from deployer
 export default apiUrl
