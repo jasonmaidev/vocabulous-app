42bbae24fe66abb6c1c39afc1268a60b430ab201
JFMai90 <jasonmaif@gmail.com>
2024-09-23 19:39:46 -0700

    refine edge case for bottom spacing

--- src/components/ViewVocabDialog.jsx
+++ src/components/ViewVocabDialog.jsx
@@ -2361,6 +2361,14 @@ const ViewVocabDialog = ({ handleViewClose, id, text, pinyinText, label, difficu
                     && expression[5]?.length > 4
                     && expression[6]?.length > 4
                   ) ||
+                  (expression[0]?.length > 4
+                    && expression[1]?.length > 4
+                    && expression[2]?.length > 4
+                    && expression[3]?.length > 4
+                    && expression[4]?.length > 4
+                    && expression[5]?.length > 4
+                    && sentence[0]?.length > 9
+                  ) ||
                   (sentence.length > 3) ? 2 : 0}
                 borderTop={`solid 1px rgba(255,255,255, 0.15)`}>
                 <Stack direction={"row"} alignItems={"center"} spacing={1}>
